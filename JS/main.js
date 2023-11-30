fa7e0afd115e14098ab0be6dc473f5f52183a391
devGiyosiddin <devgiyosiddin@gmail.com>
2023-11-30 18:49:42 +0500

    Add side-header-shopping-cart pop-up

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+const elSiteHeaderCartLink = document.querySelector('.js-site-header__card-link');
+const elSiteHeaderCartModal = document.querySelector('.site-header__cart-modal');
+
+if (elSiteHeaderCartLink) {
+    elSiteHeaderCartLink.addEventListener('click', function(evt) {
+        evt.preventDefault();
+        elSiteHeaderCartModal.classList.toggle('site-header__cart-modal--open')
+    });
+}
