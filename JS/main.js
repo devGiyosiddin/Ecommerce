2eaec3607c3314fef2f32d6d7f638d09c77f8310
devGiyosiddin <devgiyosiddin@gmail.com>
2023-12-05 20:00:29 +0500

    Js and html switch clicked image to main image

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,44 @@
 const elSiteHeaderCartLink = document.querySelector('.js-site-header__card-link');
 const elSiteHeaderCartModal = document.querySelector('.site-header__cart-modal');
 
+// Open shopping cart
 if (elSiteHeaderCartLink) {
     elSiteHeaderCartLink.addEventListener('click', function(evt) {
         evt.preventDefault();
         elSiteHeaderCartModal.classList.toggle('site-header__cart-modal--open')
     });
+};
+
+
+// Change the main img and add active item
+const elsImgShowcaseThumbnail = document.querySelectorAll('.img-showcase__thumbnail');
+const elsImgShowcaseThumbnailButton = document.querySelectorAll('.img-showcase__thumbnail-button');
+const elsImgShowcaseThumbnailImg = document.querySelectorAll('.img-showcase__thumbnail-img');
+const elImgShowcaseActiveImg = document.querySelector('.img-showcase__acitve-img');
+
+// Functions
+function deactivateImgShowcaseThumbnails () {
+    // Deactivate all active items
+    elsImgShowcaseThumbnailButton.forEach(function (elImgShowcaseThumbnailButton) {
+        elImgShowcaseThumbnailButton.parentElement.classList.remove('img-showcase__thumbnail--active')
+    })
 }
+
+
+
+
+// Switch clicked image to main image
+elsImgShowcaseThumbnailButton.forEach(function (elButton) {
+    elButton.addEventListener('click', function (evt) {
+
+        // Get the datasets value of clicked button
+        elImgShowcaseActiveImg.src = evt.target.dataset.src;
+        elImgShowcaseActiveImg.srcset = evt.target.dataset.srcset;
+
+        deactivateImgShowcaseThumbnails();
+        
+        // Add active class to clicked item
+        elButton.parentElement.classList.add('img-showcase__thumbnail--active');
+
+    });
+});
