3691e01175133c0d8bea4939c7b2cce04acce198
devGiyosiddin <devgiyosiddin@gmail.com>
2024-04-09 11:23:12 +0500

    Optimize js part 1

--- k.js
+++ k.js
@@ -2,19 +2,79 @@ const modifiers = {
     siteHadercartModalOpen: 'site-header__cart-modal--open',
     imgThumbnailActive: 'img-showcase__thumbnail--active',
     lightboxOpen: 'lightbox--open',
+    sitenavMenuClose: 'sitenav-menu-close',
+    sitenavListActive: 'sitenav__list--active',
+    bodyHidden: 'body-hidden',
 }
 
+// FUNCTIONS
 
-const elSiteHeaderCartLink = document.querySelector('.js-site-header__card-link');
-const elSiteHeaderCartModal = document.querySelector('.site-header__cart-modal');
+function addClassToParent(el, className) {
+    if (el) {
+        el.parentElement.classList.add(className);
+    };
+};
+function addClass(el, classname) {
+    el.classList.add(classname);
+}
+function removeClass(el, classname) {
+    el.classList.remove(classname);
+}
+function toggleClass(el, className) {
+    if (el) {
+        el.classList.toggle(className);
+    };
+};
 
-// Open shopping cart
-if (elSiteHeaderCartLink) {
+function deactivateImgShowcaseThumbnails () {
+    // Deactivate all active items
+    elsImgShowcaseThumbnailButton.forEach(function (elImgShowcaseThumbnailButton) {
+        elImgShowcaseThumbnailButton.parentElement.classList.remove(modifiers.imgThumbnailActive)
+    })
+}
+
+function deactivateLightboxActiveThumbnail() {
+    // Deactivate all active items
+    elslLightboxThumbnailButton.forEach(function (elLightboxThumbnailButton) {
+        elLightboxThumbnailButton.parentElement.classList.remove(modifiers.imgThumbnailActive)
+    })
+}
+
+function escRemove() {
+    if (elLightbox) {
+        elLightbox.classList.remove(modifiers.lightboxOpen);
+    }
+}; 
+
+function showNextImage() {
+    // Если у вас уже есть кнопка для переключения вперед, то вызываем ее событие click
+    if (elLightboxControlNext) {
+        elLightboxControlNext.click();
+    };
+};
+
+function showPrevImage() {
+    // Если у вас уже есть кнопка для переключения назад, то вызываем ее событие click
+    if (elLightboxControlPrev) {
+        elLightboxControlPrev.click();
+    };
+};
+
+function openShoppingCart() {
     elSiteHeaderCartLink.addEventListener('click', function(evt) {
         evt.preventDefault();
-        elSiteHeaderCartModal.classList.toggle(modifiers.siteHadercartModalOpen)
+        toggleClass(elSiteHeaderCartModal, modifiers.siteHadercartModalOpen)
     });
 };
+
+
+// Open shopping cart
+const elSiteHeaderCartLink = document.querySelector('.js-site-header__card-link');
+const elSiteHeaderCartModal = document.querySelector('.site-header__cart-modal');
+if (elSiteHeaderCartLink) {
+    openShoppingCart();
+};
+
 
 
 /* Sitenav Menu modal */
@@ -22,9 +82,9 @@ const elSitenavBtn = document.querySelector('.sitenav-button');
 const elSitenav = document.querySelector('.sitenav__list');
 if (elSitenavBtn) {
     elSitenavBtn.addEventListener('click', function () {
-        elSitenavBtn.classList.toggle('sitenav-menu-close');
-        elSitenav.classList.toggle('sitenav__list--active');
-        document.body.classList.toggle("body-hidden");
+        toggleClass(elSitenavBtn, modifiers.sitenavMenuClose);
+        toggleClass(elSitenav, modifiers.sitenavListActive);
+        toggleClass(document.body, modifiers.bodyHidden);
     });
 };
 
@@ -33,28 +93,40 @@ const elProductPageGaleery = document.querySelector('.product-page');
 const elImgShowcaseActiveImg = elProductPageGaleery.querySelector('.js-img-showcase__acitve-img');
 const elsImgShowcaseThumbnail = elProductPageGaleery.querySelectorAll('.js-img-showcase__thumbnail');
 const elsImgShowcaseThumbnailButton = elProductPageGaleery.querySelectorAll('.js-img-showcase__thumbnail-button');
-
-// Functions
-function deactivateImgShowcaseThumbnails () {
-    // Deactivate all active items
-    elsImgShowcaseThumbnailButton.forEach(function (elImgShowcaseThumbnailButton) {
-        elImgShowcaseThumbnailButton.parentElement.classList.remove(modifiers.imgThumbnailActive)
-    })
-}
+// Lightbox Img Showcase
+const elLightboxActiveImg = document.querySelector('.js-lightbox-img-showcase__acitve-img');
+const elslLightboxThumbnail = document.querySelectorAll('.js-lightbox-img-showcase__thumbnail');
+const elslLightboxThumbnailButton = document.querySelectorAll('.js-lightbox-img-showcase__thumbnail-button');
 
 
-// Switch clicked image to main image
-elsImgShowcaseThumbnailButton.forEach(function (elButton) {
-    elButton.addEventListener('click', function (evt) {
+
+// Switch clicked image to main image on Img Showcase
+elsImgShowcaseThumbnailButton.forEach(function (elbtn) {
+    elbtn.addEventListener('click', function (evt) {
         
         // Get the datasets value of clicked button
         elImgShowcaseActiveImg.src = evt.target.dataset.src;
         elImgShowcaseActiveImg.srcset = evt.target.dataset.srcset;
         
         deactivateImgShowcaseThumbnails();
-        
+
         // Add active class to clicked item
-        elButton.parentElement.classList.add(modifiers.imgThumbnailActive);
+        addClassToParent(elbtn, modifiers.imgThumbnailActive)
+    });
+});
+
+// Switch clicked image to main image on Lightbox
+elslLightboxThumbnailButton.forEach(function (elBtn) {
+    elBtn.addEventListener('click', function (evt) {
+        
+        // Get the datasets value of clicked button
+        elLightboxActiveImg.src = evt.target.dataset.src;
+        elLightboxActiveImg.srcset = evt.target.dataset.srcset;
+        
+        deactivateLightboxActiveThumbnail();
+
+        // Add active class to clicked item
+        addClassToParent(elBtn, modifiers.imgThumbnailActive)
     });
 });
 
@@ -66,44 +138,15 @@ const elLightboxOpener = document.querySelector('.js-lightbox__toggler');
 
 if (elLightboxOpener) {
     elLightboxOpener.addEventListener('click', function () {
-        elLightbox.classList.add(modifiers.lightboxOpen);
+        addClass(elLightbox, modifiers.lightboxOpen)
     });
 };
 
 if (elCloseLightbox) {
-    elCloseLightbox.addEventListener('click', function() {
-        elLightbox.classList.remove(modifiers.lightboxOpen);
+    elCloseLightbox.addEventListener('click', function () {
+        removeClass(elLightbox, modifiers.lightboxOpen)
     })
 }
-
-
-// Lightbox Img Showcase
-const elLightboxActiveImg = document.querySelector('.js-lightbox-img-showcase__acitve-img');
-const elslLightboxThumbnail = document.querySelectorAll('.js-lightbox-img-showcase__thumbnail');
-const elslLightboxThumbnailButton = document.querySelectorAll('.js-lightbox-img-showcase__thumbnail-button');
-
-// Functions
-function deactivateLightboxActiveThumbnail() {
-    // Deactivate all active items
-    elslLightboxThumbnailButton.forEach(function (elLightboxThumbnailButton) {
-        elLightboxThumbnailButton.parentElement.classList.remove(modifiers.imgThumbnailActive)
-    })
-}
-
-// Switch clicked image to main image
-elslLightboxThumbnailButton.forEach(function (elLightboxThumbnailButton) {
-    elLightboxThumbnailButton.addEventListener('click', function (evt) {
-        
-        // Get the datasets value of clicked button
-        elLightboxActiveImg.src = evt.target.dataset.src;
-        elLightboxActiveImg.srcset = evt.target.dataset.srcset;
-        
-        deactivateLightboxActiveThumbnail();
-        
-        // Add active class to clicked item
-        elLightboxThumbnailButton.parentElement.classList.add(modifiers.imgThumbnailActive);
-    });
-});
 
 
 // Lightbox Controls
@@ -234,11 +277,6 @@ if (elLightboxControlPrev2) {
 };
 
 // Remove open class on press key ESC
-function escRemove() {
-    if (elLightbox) {
-        elLightbox.classList.remove(modifiers.lightboxOpen);
-    }
-}; 
 document.addEventListener('keydown', function (event) {
     // Check if the pressed key is 'Escape'
     if (event.key === 'Escape') {
@@ -284,19 +322,7 @@ document.addEventListener('keydown', function (event) {
     }
 });
 
-function showNextImage() {
-    // Если у вас уже есть кнопка для переключения вперед, то вызываем ее событие click
-    if (elLightboxControlNext) {
-        elLightboxControlNext.click();
-    };
-};
 
-function showPrevImage() {
-    // Если у вас уже есть кнопка для переключения назад, то вызываем ее событие click
-    if (elLightboxControlPrev) {
-        elLightboxControlPrev.click();
-    };
-};
 
 
 // ADD ITEM
@@ -311,10 +337,10 @@ if (elAddtoCartBtn) {
             
             const elProductQuantity = parseInt(document.querySelector('.js-product__quantity').textContent, 10);
             const elProductPriceNumber = document.querySelector('.js-product__price-number').textContent;
-    
+            
             elShoppingCartModal.classList.remove('shopping-cart--empty');
             const lastPrice = elProductPriceNumber * elProductQuantity;
-    
+            
             elShoppingCartInner.innerHTML = `
             <!-- has-items -->
             <div class="shopping-cart__content">
@@ -326,7 +352,7 @@ if (elAddtoCartBtn) {
             <div class="item-shopping-cart-product__name">Fall Limited Edition Sneakers</div>
             <div class="item-shopping-cart-product__cost">$125.00 x ${elProductQuantity} <b class="item-shopping-cart-product__subtotal">$${lastPrice}</b></div>
             </div>
-            <button class="item-shopping-cart-product__remove-button" type="button" aria-label="remove item">
+            <button class="item-shopping-cart-product__remove-button js-shopping-cart__remove-button" type="button" aria-label="remove item">
             <svg class="item-shopping-cart-product__remove-icon" xmlns="http://www.w3.org/2000/svg" width="14" height="16" fill="none">
             <path fill="currentColor" fill-rule="evenodd"
             d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4 6c.275 0 .5.225.5.5v7c0 .275-.225.5-.5.5a.501.501 0 0 1-.5-.5v-7c0-.275.225-.5.5-.5Zm3.5.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7ZM10 6c.275 0 .5.225.5.5v7c0 .275-.225.5-.5.5a.501.501 0 0 1-.5-.5v-7c0-.275.225-.5.5-.5Z"
@@ -348,15 +374,43 @@ if (elAddtoCartBtn) {
             </a>
             </div>
             `;
-    
+            
             // Update cart link
             const count = document.createElement('span');
             count.classList.add('site-header__cart-product-count');
             elSiteHeaderCartLink.appendChild(count);
-    
+            
             // Update product count
             const elProductCount = document.querySelector('.site-header__cart-product-count');
             elProductCount.textContent = elShoppingCartCount;
-        }
+            
+            // Added item on shopping cart
+            const elAddedItem = document.querySelector('.shopping-cart__product-item');
+            if (elAddedItem) {
+                
+                const elsShoppingCartRemoveBtn = document.querySelectorAll('.js-shopping-cart__remove-button');
+                elsShoppingCartRemoveBtn.forEach(function (removeBtn) {
+
+                    // Remove item
+                    removeBtn.addEventListener('click', function (evt) {
+                        let elParentItem = evt.target.parentNode.parentNode.parentNode.parentNode.parentElement.parentNode;
+                        // Remove item from DOM
+                        elParentItem.remove();
+
+                        // Update shopping cart count
+                        const elShoppingCartCount = document.querySelector('.js-shopping-cart__inner').childElementCount;
+                        if(elShoppingCartCount === 0) {
+                            elSiteHeaderCartLink.children[1].remove();
+                        };
+
+                        // Add empty text when no items
+                        if (elShoppingCartInner.childNodes) {
+                            elShoppingCartInner.innerHTML = '<div class="shopping-cart__empty js-shopping-cart__empty">Your cart is empty.</div>';
+                            elShoppingCartModal.classList.add('shopping-cart--empty');
+                        }
+                    });
+                });
+            };    
+        };
     });
 };
